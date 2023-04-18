43e04963267250bb713c025a651d79e2faa89399
Diego Herrera <diegoricardoherrera@hotmail.com>
2023-04-18 10:36:14 -0700

    Add project files.

---
+++ ePortafolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ePortafolio.Startup))]
+namespace ePortafolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
